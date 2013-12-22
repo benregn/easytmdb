f699a1165ac889af4345dd26fe52149d52dbaf59
Tomas Thor Jonsson <benregn@gmail.com>
2013-12-22 07:54:10 +0100

    import all to tmdbsimple namespace

---
+++ tmdbsimple/__init__.py
@@ -0,0 +1,9 @@
+from .base import *
+from .search import Search
+from .discover import Discover
+from .movies import Movies, Collections, Companies, Genres, Keywords, Reviews
+from .conf_auth import Authentication, Configuration
+from .changes import Changes
+from .account import Account, Lists
+from .people import People, Credits, Jobs
+from .tv import TV, TVSeasons, TVEpisodes
