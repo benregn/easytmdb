8d8c7f35787347b4c13fee59c6d1822dd2b1140c
Tomas Thor Jonsson <benregn@gmail.com>
2014-01-22 17:47:07 +0100

    forgot to change tests to plural as well

--- tests/test_changes.py
+++ tests/test_changes.py
@@ -7,10 +7,10 @@ from easytmdb.changes import Changes
 class TestChanges(unittest.TestCase):
     def test_movie(self):
         changes = Changes()
-        changes.movie()
+        changes.movies()
         self.assertTrue(hasattr(changes, 'results'))
 
     def test_person(self):
         change = Changes()
-        change.movie()
+        change.movies()
         self.assertTrue(hasattr(change, 'results'))
